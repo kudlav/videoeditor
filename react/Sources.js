d78fffad108b3065cb5a98e45c2d700c757e2207
kudlav <vladankudlac@gmail.com>
2019-04-10 00:48:09 +0200

    Display uploaded files using React

--- react/Sources.js
+++ react/Sources.js
@@ -7,20 +7,22 @@ export default class Sources extends Component {
 		this.state = { items: this.props.items };
 	}
 
-	render() {
-		const { items } = this.props;
+	addResource(resource) {
+		const items = this.state.items.slice();
+		items.push(resource);
+		this.setState({items: items});
+	}
 
+	render() {
 		return (
 			<>
             <h3><i className="material-icons" aria-hidden="true">video_library</i>Seznam záběrů</h3>
 			<table>
 				<tbody>
-					{items.map(row => {
-						<SourcesTableRow row={row} />
-					})}
+					{this.state.items.map(item => <SourcesTableRow key={item.id} value={item} />)}
 					<tr>
 						<td colSpan="3">
-							<Uploader />
+							<Uploader value={resource => this.addResource(resource)} />
 						</td>
 					</tr>
 				</tbody>
@@ -30,24 +32,43 @@ export default class Sources extends Component {
 	}
 }
 
-const SourcesTableRow = ({row}) => (
-	<tr>/*
-		<td key={row.name}>{row.name}</td>
-		<td key={row.id}>{row.id}</td>
-		<td key={row.price}>{row.price}</td>
-	*/</tr>
+const SourcesTableRow = props => (
+	<tr>
+		<td>
+			<div><i className="material-icons resource-preview" aria-hidden="true">panorama</i></div>
+		</td>
+		<td>
+			{props.value.name}<br/>
+			<small>Délka: {props.value.duration}</small>
+		</td>
+		<td className="column-right">
+			<a><i className="material-icons" aria-hidden="true">control_point</i></a>
+			<a><i className="material-icons" aria-hidden="true">delete</i></a>
+		</td>
+	</tr>
 );
 
 class Uploader extends Component {
 
-	getUploadParams({ meta }) {
+	constructor(props) {
+		super(props);
+		this.handleChangeStatus = this.handleChangeStatus.bind(this);
+	}
+
+	static getUploadParams() {
 		return { url: '/api/project/1234/file' };
 	}
 
 	handleChangeStatus({ meta, xhr, remove }, status) {
 		if (status === 'done') {
 			console.log(`${meta.name} uploaded!`);
-			console.log(xhr);
+			const response = JSON.parse(xhr.response);
+			this.props.value({
+				id: response.resource_id,
+				name: meta.name,
+				duration: response.length,
+				mime: response.resource_mime,
+			});
 			remove();
 		} else if (status === 'aborted') {
 			console.log(`${meta.name}, upload failed...`);
@@ -57,7 +78,7 @@ class Uploader extends Component {
 	render () {
 		return (
 			<Dropzone
-				getUploadParams={this.getUploadParams}
+				getUploadParams={Uploader.getUploadParams}
 				onChangeStatus={this.handleChangeStatus}
 				accept="image/*,audio/*,video/*"
 				inputContent={(files, extra) => (extra.reject ? 'Nahrávat lze pouze video, audio a obrázkové soubory.' : 'Nahrát soubory')}
