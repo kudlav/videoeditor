11f2b57285bf6090f4aa5525cc8742534e4e9788
kudlav <vladankudlac@gmail.com>
2019-04-10 16:03:59 +0200

    Config cleanup

--- config.js
+++ config.js
@@ -1,18 +1,11 @@
-const env = process.env;
-
-export const nodeEnv = env.NODE_ENV || 'development';
-
-export const logStars = (message) => {
-	console.info('**********');
-	console.info(message);
-	console.info('**********');
-};
-
 export default {
-	port: env.PORT || 8080,
-	host: env.HOST || '0.0.0.0',
+	port: 8080,
+	host: 'localhost',
 	get serverUrl() {
 		return `http://${this.host}:${this.port}`;
+	},
+	get apiPath() {
+		return `http://${this.host}:${this.port}/api`;
 	},
 
 	projectPath: 'WORKER',
