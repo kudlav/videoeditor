08ddab1ca573ddd3d05abeee5b64cab1aaaae8bd
kudlav <vladankudlac@gmail.com>
2019-04-17 14:18:40 +0200

    Catch and log all errors and exceptions

---
+++ controllers/errorController.js
@@ -0,0 +1,12 @@
+/**
+ * @file Controller for handling errors and exceptions
+ * @author Vladan Kudlac <vladankudlac@gmail.com>
+ */
+
+exports.default = (err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500);
+	res.json({
+			msg: 'Internal server error occured.',
+	});
+};
--- router.js
+++ router.js
@@ -4,6 +4,7 @@ const router = server.Router();
 // Require controller modules.
 const mainController = require('./controllers/mainController');
 const apiController = require('./controllers/apiController');
+const errorController = require('./controllers/errorController');
 
 // Log access
 router.use((req, res, next) => {
@@ -33,5 +34,8 @@ router.post('/api/project/:projectID/filter', apiController.projectFilterPOST);
 router.delete('/api/project/:projectID/filter', apiController.projectFilterDELETE);
 
 router.post('/api/project/:projectID/transition', apiController.projectTransitionPOST);
+
+// Error handling
+router.use(errorController.default);
 
 module.exports = router;
