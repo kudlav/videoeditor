e29611e6e9bab069bbf3a77b3b63724973b417c5
kudlav <vladankudlac@gmail.com>
2019-04-28 00:56:25 +0200

    Email contains code for embedding video

--- models/emailManager.js
+++ models/emailManager.js
@@ -31,13 +31,16 @@ export default {
 		};
 
 		if (success) {
-			email.html = `Právě bylo dokončeno zpracování výsledného videa vašeho <a href="${projectLink}">projektu</a>.
-				<br>Stáhnout si jej můžete na následujícím odkazu: <a href="${videoLink}">${videoLink}</a>`;
+			email.html = `<p>Právě bylo dokončeno zpracování výsledného videa vašeho <a href="${projectLink}">projektu</a>.</p>
+				<p>Stáhnout si jej můžete na následujícím odkazu: <a href="${videoLink}">${videoLink}</a></p>
+				<p>Kód pro vložení videa do stránky:</p>
+				<pre>&lt;video controls src="${videoLink}" /&gt;</pre>`
+			;
 		}
 		else {
 			email.to += `, ${config.adminEmail}`;
-			email.html = `Váš <a href="${projectLink}">projekt</a> nemohl být zpracován.
-				<br>Omlouváme se za způsobené komplikace, problémem se budeme co nejdříve zabývat.`;
+			email.html = `<p>Váš <a href="${projectLink}">projekt</a> nemohl být zpracován.</p>
+				<p>Omlouváme se za způsobené komplikace, problémem se budeme co nejdříve zabývat.</p>`;
 		}
 
 		transporter.sendMail(email, (err) => {
