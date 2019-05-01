d8a039e0d485e56296cadba0d7f21cf754588e4e
kudlav <vladankudlac@gmail.com>
2019-05-01 23:18:19 +0200

    Customize ESLint indent config

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
 	},
 	"plugins": [ "react" ],
 	"rules": {
-		"indent": ["error", 2],
+		"indent": ["error", "tab", {"SwitchCase": 1}],
 		"linebreak-style": ["error","unix"],
 		"quotes": ["error","single"],
 		"semi": ["error","always"],
