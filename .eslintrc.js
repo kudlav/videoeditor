ac01426f70cc1652240b6d8a22085f3417653826
kudlav <vladankudlac@gmail.com>
2019-06-10 21:00:34 +0200

    Use simple quotes in eslintrc, strict no-console

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,29 @@
 module.exports = {
-	"parser": 'babel-eslint',
-	"env": {
-		"browser": true,
-		"commonjs": true,
-		"es6": true,
-		"node": true
+	'parser': 'babel-eslint',
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es6': true,
+		'node': true
 	},
-	"extends": ["eslint:recommended", "plugin:react/recommended"],
-	"parserOptions": {
-		"ecmaFeatures": {
-			"experimentalObjectRestSpread": true,
-			"jsx": true
+	'extends': ['eslint:recommended', 'plugin:react/recommended'],
+	'parserOptions': {
+		'ecmaFeatures': {
+			'experimentalObjectRestSpread': true,
+			'jsx': true
 		},
-		"sourceType": "module"
+		'sourceType': 'module'
 	},
-	"plugins": [ "react" ],
-	"rules": {
-		"indent": ["error", "tab", {"SwitchCase": 1}],
-		"linebreak-style": ["error","unix"],
-		"quotes": ["error","single"],
-		"semi": ["error","always"],
-		"no-console": ["warn", { "allow": ["info", "error"] }]
+	'plugins': [ 'react' ],
+	'rules': {
+		'indent': ['error', 'tab', {'SwitchCase': 1}],
+		'linebreak-style': ['error','unix'],
+		'quotes': ['error','single'],
+		'semi': ['error','always'],
+	},
+	'settings': {
+		'react': {
+			'version': 'detect',
+		}
 	}
 };
